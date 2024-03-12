ddaec4c2cb94bb1b0dd14e75af7b9b2a46fc4e77
yuqihu1103 <hu.yuqi@northeastern.edu>
2024-03-12 16:33:27 -0700

    create project in Xcode

---
+++ SmokeFreeLife/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmokeFreeLife
+//
+//  Created by XuanShichun on 11/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
